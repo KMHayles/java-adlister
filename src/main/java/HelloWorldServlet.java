78b567b3047f55f8a377f9f0cfa6a9ebbda81360
Kenneth Hayles <kenneth.m.hayles@gmail.com>
2023-03-09 10:33:41 -0600

    servlets-exercise testing

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,17 @@
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
 
+@WebServlet(name = "HelloWorldServlet", value = "/hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = request.getParameter("Kenneth");
+        System.out.println(name);
+
+        response.getWriter().println("Hello, " + name);
 
     }
+
 }
