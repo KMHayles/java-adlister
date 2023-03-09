a590e4f6c928f7d0fbaa2c8ece942658d6e9a008
Kenneth Hayles <kenneth.m.hayles@gmail.com>
2023-03-09 09:05:40 -0600

    Testing servlets with test branch

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,18 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+
+//user goes to local host 8080/hell in the browser and sees "Hello World"
+@WebServlet("/hello") //this is what tells the below code to run.
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
-
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        PrintWriter out = response.getWriter();
+        out.println("<h1 style='color:red'>Test Hello</h1><br><h3>This is a h3</h3><a href='/login'>Login Page</a>");
+//        System.out.println("Hello World");
     }
 }
