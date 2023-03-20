61f30248cbdf2b242276057fde7e75a7f4427db6
Kenneth Hayles <kenneth.m.hayles@gmail.com>
2023-03-20 01:22:24 -0500

    password exercise need user login security logic

--- src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
@@ -1,6 +1,7 @@
 package com.codeup.adlister.dao;
 
 import com.codeup.adlister.models.User;
+import com.codeup.adlister.util.Password;
 import com.mysql.cj.jdbc.Driver;
 
 import java.sql.*;
@@ -41,7 +42,7 @@ public class MySQLUsersDao implements Users {
             PreparedStatement stmt = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
             stmt.setString(1, user.getUsername());
             stmt.setString(2, user.getEmail());
-            stmt.setString(3, user.getPassword());
+            stmt.setString(3, Password.hash(user.getPassword()));
             stmt.executeUpdate();
             ResultSet rs = stmt.getGeneratedKeys();
             rs.next();
@@ -62,5 +63,11 @@ public class MySQLUsersDao implements Users {
             rs.getString("password")
         );
     }
+
+//    public static void main(String[] args) {
+//        String password = Password.hash("password");
+//        System.out.println(password);
+//        System.out.println(Password.check("password", Password.hash("password")));
+//    }
 
 }
